4230d51a0d631cafdcdb66c8a06f4ea29fae2482
johndoeadams <johndoeadams@gmail.com>
2018-12-22 13:09:27 -0500

    update js

--- assets/js/run.js
+++ assets/js/run.js
@@ -1,9 +1,51 @@
 
 
 //variables
-
+var topics=[];
 //objects
 
 //functions
+
+
+//collects the input from the input field and adds it to the array
+function collectInput(){
+//check if the input is already in the array
+//alert if already in the array
+
+//add text into the array
+}
+
+//generates a single button
+function generateButton(){
+
+  //create a variable to hold a button tag
+
+  //add unique ID attrubite to the button
+
+  //append button to the div
+
+
+
+}
+
+
+
+//When the user clicks on a button, the page should grab 10 static,
+// non-animated gif images from the GIPHY API and place them on the page.
+
+//
+// When the user clicks one of the still GIPHY images,
+// the gif should animate.
+// If the user clicks the gif again, it should stop playing.
+//
+// Under every gif, display its rating (PG, G, so on).
+//    * This data is provided by the GIPHY API.
+//    * Only once you get images displaying with button presses should you move on to the next step.
+
+
+
+
+
+
 
 //other stuff
