3e22b72c5bcdb7968111acf5f6cc6dbc6ad3f58e
johndoeadams <johndoeadams@gmail.com>
2019-01-05 17:52:32 -0500

    update JS again

--- assets/js/run.js
+++ assets/js/run.js
@@ -6,17 +6,18 @@ var term;
 var topTen=[];
 // ajax stuff
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        term + "&api_key=dc6zaTOxFJmzC&limit=10";
-
 generateButton();
-
 
 //click search and perform ajax call
 $("#search").click(function(){
-  collectInput();
   term=$("#input1").val().trim();
-  // $("#input1").val("");
+  topics.push(term);
+  generateButton(topics);
+  console.log(topics);
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          term + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -27,46 +28,29 @@ $("#search").click(function(){
     var results = response.data;
 
     for (var i = 0; i <results.length; i++) {
-      // var gifDiv = $("<div>");
-      // gifDiv.addClass("col-lg-6 w-50 p-3");
       var personImage = $("<img>");
       personImage.attr("src", results[i].images.fixed_height.url);
-      // gifDiv.append(personImage);
       $(".gifsHere").prepend(personImage);
     };
   });
 });
 
-//objects
+$("#btn btn-danger").click(function(){
+  $(".buttons text-center").empty();
+  $(".gifsHere").empty();
+});
 
-//functions
-
-
-//collects the input from the input field and adds it to the array
-function collectInput(){
-//check if the input is already in the array
-
-term = $("#input1").val().toLowerCase();
-console.log(term);
-
-if(topics.includes(term) && term!=null){
-
-
-  }
-  else{
-    //add text into the array
-  topics.push(term);
-  }
-}
 
 //generates a single button
 function generateButton(item){
-topics.forEach(function(index){
   //create a variable to hold a button tag
-var temp =$("<button>");
-  //add unique ID attrubite to the button
-temp.attr("data-name", index);
+  if(term!=null){
+    var temp =$("<button>"+term+"</button>");
+      //add unique ID attrubite to the button
+    temp.attr("data-name", item);
+    $(".buttons").append(temp);
+  }
 
-});
+
 
 }
